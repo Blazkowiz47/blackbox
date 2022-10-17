545d8e5b40e24018727638d55ae26fda79e67d48
Vidit-Ostwal <viditostwal@gmail.com>
2022-10-17 11:26:16 +0530

    commits

--- Regression/utils/initialisation.py
+++ Regression/utils/initialisation.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def DenseLayer(input_dim, output=1, initialiser='random', bias_initialiser='zero'):
-    return __initialiser__(initialiser, (input_dim,output)), __initialiser__(bias_initialiser, (1,output))
+def DenseLayer(input_dim, output=1, weight_initialiser='random', bias_initialiser='zero'):
+    return __initialiser__(weight_initialiser, (input_dim,output)), __initialiser__(bias_initialiser, (1,output))
 
 
 
